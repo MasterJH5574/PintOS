405bf876aa4c4c33faefbb158173af1bc3cf1751
MasterJH5574 <45167100+MasterJH5574@users.noreply.github.com>
2020-06-25 18:09:54 +0800

    remove changes in pagedir.c

--- pintos/src/userprog/pagedir.c
+++ pintos/src/userprog/pagedir.c
@@ -42,11 +42,7 @@ pagedir_destroy (uint32_t *pd)
         
         for (pte = pt; pte < pt + PGSIZE / sizeof *pte; pte++)
           if (*pte & PTE_P)
-            #ifdef VM
-              frame_free_frame(pte_get_page (*pte));
-            #else
               palloc_free_page (pte_get_page (*pte));
-            #endif
             
         palloc_free_page (pt);
       }
