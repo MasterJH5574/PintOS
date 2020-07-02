aeb15f396bc74d23c8398f9260cece057da5da45
jinhongyi <3231950289@qq.com>
2020-07-02 23:12:59 +0800

    change load segment for sharing

--- pintos/src/filesys/file.c
+++ pintos/src/filesys/file.c
@@ -17,13 +17,7 @@ bool file_descriptor_is_dir(struct file_descriptor *fd) {
 /* Ruihang End */
 
 /* An open file. */
-struct file 
-  {
-    struct inode *inode;        /* File's inode. */
-    off_t pos;                  /* Current position. */
-    bool deny_write;            /* Has file_deny_write() been called? */
-    struct dir* dir;
-};
+
 
 /* Opens a file for the given INODE, of which it takes ownership,
    and returns the new file.  Returns a null pointer if an
