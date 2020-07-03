4f676cf6bfd0fb544563ca46cc8be9df190d8873
ZYHowell <zhuangyh@sjtu.edu.cn>
2020-07-03 20:28:09 +0800

    frame turn thread_hold to a lislist

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -1,6 +1,15 @@
 #ifndef THREADS_THREAD_H
 #define THREADS_THREAD_H
 
+#ifndef USERPROG
+#define USERPROG
+#endif
+#ifndef VM
+#define VM
+#endif
+#ifndef FILESYS
+#define FILESYS
+#endif
 #include <debug.h>
 #include <list.h>
 #include <stdint.h>
